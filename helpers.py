64acb77092d56f8a0e607fead5d47d1f1c025a42
JayThibs <thibo.jacques@gmail.com>
2021-08-15 18:21:09 +0000

    Added raw text to our dataframe to use for multimodal bert model.

--- helpers.py
+++ helpers.py
@@ -67,12 +67,19 @@ def train_test_dataframe(clean_df, random_seed=100):
 
 
 # helper function for pre-processing text given a file
-def process_file(file):
-    # put text in all lower case letters 
-    all_text = file.read()#.lower()
+def process_file(file, raw=False):
+    all_text = file.read()
+    
+    if raw == False:
+        # put text in all lower case letters 
+        all_text = all_text.lower()
 
-    # remove all non-alphanumeric chars
-    all_text = re.sub(r"[^a-zA-Z0-9]", " ", all_text)
+        # remove all non-alphanumeric chars
+        all_text = re.sub(r"[^a-zA-Z0-9]", " ", all_text)
+    
+    if raw == True:
+        all_text = re.sub(r'[^\w\s,\-\']+', '', all_text)
+    
     # remove newlines/tabs, etc. so it's easier to match phrases, later
     all_text = re.sub(r"\t", " ", all_text)
     all_text = re.sub(r"\n", " ", all_text)
@@ -108,5 +115,35 @@ def create_text_column(df, file_directory='data/'):
     
     # add column to the copied dataframe
     text_df['Text'] = text
+    
+    return text_df
+
+
+def create_raw_text_column(df, file_directory='data/'):
+    '''Reads in the files, listed in a df and returns that df with an additional column, `Text`. 
+       :param df: A dataframe of file information including a column for `File`
+       :param file_directory: the main directory where files are stored
+       :return: A dataframe with processed text '''
+   
+    # create copy to modify
+    text_df = df.copy()
+    
+    # store processed text
+    text = []
+    
+    # for each file (row) in the df, read in the file 
+    for row_i in df.index:
+        filename = df.iloc[row_i]['File']
+        #print(filename)
+        file_path = file_directory + filename
+        with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
+
+            # standardize text using helper function
+            file_text = process_file(file, raw=True)
+            # append processed text to list
+            text.append(file_text)
+    
+    # add column to the copied dataframe
+    text_df['raw_Text'] = text
     
     return text_df
