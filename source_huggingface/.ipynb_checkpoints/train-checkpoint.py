646959eb18551cbffc61234728c92e2af32559db
JayThibs <thibo.jacques@gmail.com>
2021-08-18 19:05:21 +0000

    Cleaned up train.py.

--- source_huggingface/.ipynb_checkpoints/train-checkpoint.py
+++ source_huggingface/.ipynb_checkpoints/train-checkpoint.py
@@ -27,8 +27,6 @@ from multimodal_transformers.model import AutoModelWithTabular
 
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
-
-
 # Set up logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -38,92 +36,6 @@ logging.basicConfig(
 )
 
 os.environ['COMET_MODE'] = 'DISABLED'
-
-
-
-# imports the model in model.py by name
-from model import BinaryClassifier
-
-def model_fn(model_dir):
-    """Load the PyTorch model from the `model_dir` directory."""
-    print("Loading model.")
-
-    # First, load the parameters used to create the model.
-    model_info = {}
-    model_info_path = os.path.join(model_dir, 'model_info.pth')
-    with open(model_info_path, 'rb') as f:
-        model_info = torch.load(f)
-
-    print("model_info: {}".format(model_info))
-
-    # Determine the device and construct the model.
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = BinaryClassifier(model_info['input_features'], model_info['hidden_dim'], model_info['output_dim'])
-
-    # Load the stored model parameters.
-    model_path = os.path.join(model_dir, 'model.pth')
-    with open(model_path, 'rb') as f:
-        model.load_state_dict(torch.load(f))
-
-    # set to eval mode, could use no_grad
-    model.to(device).eval()
-
-    print("Done loading model.")
-    return model
-
-# Gets training data in batches from the train.csv file
-def _get_train_data_loader(batch_size, training_dir):
-    print("Get train data loader.")
-
-    train_data = pd.read_csv(os.path.join(training_dir, "train.csv"), header=None, names=None)
-
-    train_y = torch.from_numpy(train_data[[0]].values).float().squeeze()
-    train_x = torch.from_numpy(train_data.drop([0], axis=1).values).float()
-
-    train_ds = torch.utils.data.TensorDataset(train_x, train_y)
-
-    return torch.utils.data.DataLoader(train_ds, batch_size=batch_size)
-
-
-# Provided training function
-def train(model, train_loader, epochs, criterion, optimizer, device):
-    """
-    This is the training method that is called by the PyTorch training script. The parameters
-    passed are as follows:
-    model        - The PyTorch model that we wish to train.
-    train_loader - The PyTorch DataLoader that should be used during training.
-    epochs       - The total number of epochs to train for.
-    criterion    - The loss function used for training. 
-    optimizer    - The optimizer to use during training.
-    device       - Where the model and data should be loaded (gpu or cpu).
-    """
-    
-    # training loop is provided
-    for epoch in range(1, epochs + 1):
-        model.train() # Make sure that the model is in training mode.
-
-        total_loss = 0
-
-        for batch in train_loader:
-            # get data
-            batch_x, batch_y = batch
-
-            batch_x = batch_x.to(device)
-            batch_y = batch_y.to(device)
-
-            optimizer.zero_grad()
-
-            # get predictions from model
-            y_pred = model(batch_x)
-            
-            # perform backprop
-            loss = criterion(y_pred, batch_y)
-            loss.backward()
-            optimizer.step()
-            
-            total_loss += loss.data.item()
-
-        print("Epoch: {}, Loss: {}".format(epoch, total_loss / len(train_loader)))
 
 
 if __name__ == '__main__':
